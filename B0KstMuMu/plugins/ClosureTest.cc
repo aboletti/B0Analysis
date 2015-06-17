82ad6461a75eade2003fa32e57a5b8f0327ff6b6
Stefano Lacaprara <stefano.lacaprara@pd.infn.it>
2015-06-17 15:43:28 +0200

    working version with 2D histogram and variable binning

--- B0KstMuMu/plugins/ClosureTest.cc
+++ B0KstMuMu/plugins/ClosureTest.cc
@@ -13,6 +13,7 @@
 #include "RooRealVar.h"
 #include "RooDataHist.h"
 #include "RooPlot.h"
+#include "RooBinning.h"
 
 using namespace RooFit ;
 #include <TApplication.h>
@@ -22,40 +23,73 @@ using namespace RooFit ;
 #include "TMath.h"
 
 // ####################
+// # Global constants #
+// ####################
+#define SETBATCH        false
+#define PARAMETERFILEIN "/python/ParameterFile.txt"
+#define ordinateRange   1e-2
+
+// ####################
 // # Global variables #
 // ####################
 Utils* Utility;
+
+vector<double> q2Bins;
+vector<double> cosThetaKBins;
+vector<double> cosThetaLBins;
+vector<double> phiBins;
 
 void loadHisto() {
   // These are the 2D histogram of the efficiency. Try to save them somewhere
   TFile* effHistoFile=TFile::Open("effHisto2DFile.root","READ");
   cout << "Loading histos" ;
-  TH2D* H2Deff_0_q2Bin_Org_1=(TH2D*) effHistoFile->Get("H2Deff_0_q2Bin_Org_1");
-  TH2D* H2Deff_1_q2Bin_Org_1=(TH2D*) effHistoFile->Get("H2Deff_1_q2Bin_Org_1");
-  TH2D* H2Deff_2_q2Bin_Org_1=(TH2D*) effHistoFile->Get("H2Deff_2_q2Bin_Org_1");
+  TH2D* H2Deff_0_q2Bin_Org_2=(TH2D*) effHistoFile->Get("H2Deff_0_q2Bin_Org_2");
+  TH2D* H2Deff_1_q2Bin_Org_2=(TH2D*) effHistoFile->Get("H2Deff_1_q2Bin_Org_2");
+  TH2D* H2Deff_2_q2Bin_Org_2=(TH2D*) effHistoFile->Get("H2Deff_2_q2Bin_Org_2");
   cout << " done " << endl;
-  effHistoFile->Close();
+  //effHistoFile->Close();
 
   // Define variables
-  RooRealVar ctK("ctK","cos#theta_K",-1,1);
-  RooRealVar ctL("ctL","cos#theta_L",-1,1);
-  RooRealVar phi("phi","#phi",-TMath::Pi(),TMath::Pi());
+  RooRealVar ctK("ctK","cos#theta_K",-1.,1.);
+  RooRealVar ctL("ctL","cos#theta_L",-1.,1.);
+  RooRealVar phi("phi","#phi",1.*TMath::Pi(),1.*TMath::Pi());
 
   RooArgSet ctKctLS(ctK,ctL);
   RooArgList ctKctL(ctKctLS);
 
-  cout << "creating RooDataHist" ;
+  cout << "creating RooDataHist " << H2Deff_0_q2Bin_Org_2 << endl ;
   // Create a RooDataHist from a TH2D: how?
-  RooDataHist pippo("pippo","pippo", ctKctL, H2Deff_0_q2Bin_Org_1);
+  RooDataHist pippo("pippo","pippo", ctKctL, Import(*H2Deff_0_q2Bin_Org_2,kFALSE));
   cout << " done " << endl;
 
-  // // Represent data in dh as pdf in x
-  // RooHistPdf histpdf1("histpdf1","histpdf1", ctKctL, pippo, 0);
+  // Represent data in dh as pdf in x
+  RooHistPdf histpdf1("histpdf1","histpdf1", ctKctL, pippo, 0);
 
   // Make plot of binned dataset showing Poisson error bars (RooFit default)
   // RooPlot* frame = x.frame(Title("Imported TH1 with Poisson error bars")) ;
   // dh.plotOn(frame) ;
-  TH2* hh21 = (TH2*)pippo.createHistogram("hh21",ctK,Binning(10,-1,1),YVar(ctL,Binning(10,-1,1)));
+  
+  double* q2Bins_        = Utility->MakeBinning(&q2Bins);
+  double* cosThetaKBins_ = Utility->MakeBinning(&cosThetaKBins);
+  double* cosThetaLBins_ = Utility->MakeBinning(&cosThetaLBins);
+  double* phiBins_       = Utility->MakeBinning(&phiBins);
+
+  cout << "cosThetaL binning " << endl;
+  for (vector<double>::const_iterator xlow= cosThetaLBins.begin(); xlow!=cosThetaLBins.end(); xlow++ ) cout << (*xlow) << " " << endl;
+
+  RooBinning cosThetaLBinning(cosThetaLBins.size()-1,cosThetaLBins_);
+
+  cout << "RooBinning L: " << cosThetaLBinning.lowBound() << " H: " << cosThetaLBinning.highBound() << " N " << cosThetaLBinning.numBoundaries() << endl;
+  // RooBinning cosThetaKBinning;
+  // for (vector<double>::const_iterator xlow= cosThetaKBins.begin(); xlow!=cosThetaKBins.end(); xlow++ ) cosThetaKBinning.addBoundary(*xlow);
+
+  cout << "cosThetaK binning " << endl;
+  for (vector<double>::const_iterator xlow= cosThetaKBins.begin(); xlow!=cosThetaKBins.end(); xlow++ ) cout << (*xlow) << " " << endl;
+
+  RooBinning cosThetaKBinning(cosThetaKBins.size()-1,cosThetaKBins_);
+  cout << "RooBinning L: " << cosThetaKBinning.lowBound() << " H: " << cosThetaKBinning.highBound() << " N " << cosThetaKBinning.numBoundaries() << endl;
+
+  TH2* hh21 = (TH2*)pippo.createHistogram("hh21",ctK,Binning(cosThetaKBinning),YVar(ctL,Binning(cosThetaLBinning)));
 
   // P l o t   a n d   f i t   a   R o o D a t a H i s t
   // ---------------------------------------------------
@@ -69,15 +103,16 @@ void loadHisto() {
   c->cd(1);
   gPad->SetLeftMargin(0.15) ;
   framex->GetYaxis()->SetTitleOffset(1.4) ;
-  framex->Draw("") ;
+  framex->Draw() ;
   c->cd(2);
   gPad->SetLeftMargin(0.15) ;
   framey->GetYaxis()->SetTitleOffset(1.4) ;
-  framey->Draw("") ;
+  framey->Draw() ;
   c->cd(3);
-  pippo.Draw("lego");
+  H2Deff_0_q2Bin_Org_2->Draw("lego2 fp");
   c->cd(4);
-  hh21->Draw("lego");
+  hh21->Draw("lego2 fp");
+  //effHistoFile->Close();
 }
 
 int main(int argc, char** argv)
@@ -90,7 +125,9 @@ int main(int argc, char** argv)
 
 	TApplication theApp ("Applications", &argc, argv);
 	Utility = new Utils(false);
+	Utility->ReadAllBins(Utility->MakeAnalysisPATH(PARAMETERFILEIN).c_str(),&q2Bins,&cosThetaKBins,&cosThetaLBins,&phiBins,"goodTag");
       loadHisto();
+      theApp.Run ();
     }
   else
     {
