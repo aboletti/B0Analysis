3966a64f865968aaf14225ab2714b4dc0518cf61
Stefano Lacaprara <stefano.lacaprara@pd.infn.it>
2015-06-23 15:44:19 +0200

    Produce all RooHistPdf for all q2bins

--- B0KstMuMu/plugins/ClosureTest.cc
+++ B0KstMuMu/plugins/ClosureTest.cc
@@ -14,8 +14,9 @@
 #include "RooDataHist.h"
 #include "RooPlot.h"
 #include "RooBinning.h"
-
 using namespace RooFit ;
+
+#include <TROOT.h>
 #include <TApplication.h>
 #include "TCanvas.h"
 #include "TFile.h"
@@ -35,71 +36,98 @@ using namespace RooFit ;
 Utils* Utility;
 
 vector<double> q2Bins;
-vector<double> cosThetaKBins;
-vector<double> cosThetaLBins;
-vector<double> phiBins;
+double* q2Bins_ ;
+double* cosThetaKBins_ ;
+double* cosThetaLBins_ ;
+double* phiBins_ ;
+RooBinning cosThetaLBinning;
+RooBinning cosThetaKBinning;
+RooBinning phiBinning;
+RooRealVar ctK("ctK","cos#theta_{K}",-1.,1.);
+RooRealVar ctL("ctL","cos#theta_{L}",-1.,1.);
+RooRealVar phi("phi","#phi",-1.*TMath::Pi(),1.*TMath::Pi());
+RooArgSet ctKctL(ctK,ctL);
+RooArgSet ctKphi(ctK,phi);
+RooArgSet ctLphi(ctL,phi);
 
-void loadHisto() {
+TFile* effHistoFile=0;
+TFile* pdf2DOutputFile;
+TH2D* H2Deff_ctK_ctL;
+TH2D* H2Deff_ctL_phi;
+TH2D* H2Deff_ctK_phi;
+
+void loadEffHisto(unsigned int q2bin) {
+  if (!effHistoFile) { 
+    cout << "Loading effHistoFile.root " << endl;
+    effHistoFile=TFile::Open("effHisto2DFile.root","READ");
+  }
+
+  H2Deff_ctK_ctL=(TH2D*) effHistoFile->Get(Form("H2Deff_0_q2Bin_Org_%d",q2bin));
+  H2Deff_ctL_phi=(TH2D*) effHistoFile->Get(Form("H2Deff_1_q2Bin_Org_%d",q2bin));
+  H2Deff_ctK_phi=(TH2D*) effHistoFile->Get(Form("H2Deff_2_q2Bin_Org_%d",q2bin));
+}
+
+void plotHisto(unsigned int q2bin, const RooHistPdf& xyEff, const RooDataHist& xyHisto) {
   // These are the 2D histogram of the efficiency. Try to save them somewhere
-  TFile* effHistoFile=TFile::Open("effHisto2DFile.root","READ");
-  cout << "Loading histos" ;
-  TH2D* H2Deff_0_q2Bin_Org_2=(TH2D*) effHistoFile->Get("H2Deff_0_q2Bin_Org_2");
-  TH2D* H2Deff_1_q2Bin_Org_2=(TH2D*) effHistoFile->Get("H2Deff_1_q2Bin_Org_2");
-  TH2D* H2Deff_2_q2Bin_Org_2=(TH2D*) effHistoFile->Get("H2Deff_2_q2Bin_Org_2");
-  cout << " done " << endl;
-  //effHistoFile->Close();
 
-  // Define variables
-  RooRealVar ctK("ctK","cos#theta_K",-1.,1.);
-  RooRealVar ctL("ctL","cos#theta_L",-1.,1.);
-  RooRealVar phi("phi","#phi",1.*TMath::Pi(),1.*TMath::Pi());
+  TCanvas* cOrig = new TCanvas("cOrig","Original histo",800,800) ;
+  cOrig->Divide(2,2);
+  cOrig->cd(1);
+  H2Deff_ctK_ctL->DrawCopy("lego2 fp");
+  cOrig->cd(2);
+  H2Deff_ctK_phi->DrawCopy("lego2 fp");
+  cOrig->cd(3);
+  H2Deff_ctL_phi->DrawCopy("lego2 fp");
 
-  RooArgSet ctKctLS(ctK,ctL);
-  RooArgList ctKctL(ctKctLS);
+  xyEff.Print();
+  bool isCtK=false, isCtL=false, isPhi=false;
+  if (xyEff.dependsOn(ctK)) isCtK=true;
+  if (xyEff.dependsOn(ctL)) isCtL=true;
+  if (xyEff.dependsOn(phi)) isPhi=true;
 
-  cout << "creating RooDataHist " << H2Deff_0_q2Bin_Org_2 << endl ;
-  // Create a RooDataHist from a TH2D: how?
-  RooDataHist pippo("pippo","pippo", ctKctL, Import(*H2Deff_0_q2Bin_Org_2,kFALSE));
-  cout << " done " << endl;
+  TH2* h2Orig=0;
+  TH2* hh21Histo=0;
+  TH2* hh21Pdf=0;
+  TString caName("EffPlot");
+  TString caTitle("Eff Plot vs ");
+  RooPlot* framex=0;
+  RooPlot* framey=0;
+  if (isCtK && isCtL) {
+    h2Orig=H2Deff_ctK_ctL;
+    hh21Histo = (TH2*)xyHisto.createHistogram("hh21Histo",ctK,Binning(cosThetaKBinning),YVar(ctL,Binning(cosThetaLBinning)));
+    hh21Pdf = (TH2*)xyEff.createHistogram("hh21Pdf",ctK,Binning(cosThetaKBinning),YVar(ctL,Binning(cosThetaLBinning)));
+    framex = ctK.frame(Title("cos#theta_{K} projection")) ;
+    framey = ctL.frame(Title("cos#theta_{L} projection")) ;
+    caName+=TString("CtkCtl");
+    caTitle+=TString(" cos#theta_{K},cos#theta_{L}");
+  }
+  else if (isCtK && isPhi) {
+    h2Orig=H2Deff_ctK_phi;
+    phiBinning.Print();
+    hh21Histo = (TH2*)xyHisto.createHistogram("hh21Histo",ctK,Binning(cosThetaKBinning),YVar(phi,Binning(phiBinning)));
+    hh21Pdf = (TH2*)xyEff.createHistogram("hh21Pdf",ctK,Binning(cosThetaKBinning),YVar(phi,Binning(phiBinning)));
+    framex = ctK.frame(Title("cos#theta_{K} projection")) ;
+    framey = phi.frame(Title("#phi projection")) ;
+    caName+=TString("CtkPhi");
+    caTitle+=TString(" cos#theta_{K},#phi");
+  }
+  else if (isCtL && isPhi) {
+    h2Orig=H2Deff_ctL_phi;
+    hh21Histo = (TH2*)xyHisto.createHistogram("hh21Histo",ctL,Binning(cosThetaLBinning),YVar(phi,Binning(phiBinning)));
+    hh21Pdf = (TH2*)xyEff.createHistogram("hh21Pdf",ctL,Binning(cosThetaLBinning),YVar(phi,Binning(phiBinning)));
+    framex = ctL.frame(Title("cos#theta_{L} projection")) ;
+    framey = phi.frame(Title("#phi projection")) ;
+    caName+=TString("CtlPhi");
+    caTitle+=TString(" cos#theta_{L},#phi");
+  }
 
-  // Represent data in dh as pdf in x
-  RooHistPdf histpdf1("histpdf1","histpdf1", ctKctL, pippo, 0);
-
-  // Make plot of binned dataset showing Poisson error bars (RooFit default)
-  // RooPlot* frame = x.frame(Title("Imported TH1 with Poisson error bars")) ;
-  // dh.plotOn(frame) ;
-  
-  double* q2Bins_        = Utility->MakeBinning(&q2Bins);
-  double* cosThetaKBins_ = Utility->MakeBinning(&cosThetaKBins);
-  double* cosThetaLBins_ = Utility->MakeBinning(&cosThetaLBins);
-  double* phiBins_       = Utility->MakeBinning(&phiBins);
-
-  cout << "cosThetaL binning " << endl;
-  for (vector<double>::const_iterator xlow= cosThetaLBins.begin(); xlow!=cosThetaLBins.end(); xlow++ ) cout << (*xlow) << " " << endl;
-
-  RooBinning cosThetaLBinning(cosThetaLBins.size()-1,cosThetaLBins_);
-
-  cout << "RooBinning L: " << cosThetaLBinning.lowBound() << " H: " << cosThetaLBinning.highBound() << " N " << cosThetaLBinning.numBoundaries() << endl;
-  // RooBinning cosThetaKBinning;
-  // for (vector<double>::const_iterator xlow= cosThetaKBins.begin(); xlow!=cosThetaKBins.end(); xlow++ ) cosThetaKBinning.addBoundary(*xlow);
-
-  cout << "cosThetaK binning " << endl;
-  for (vector<double>::const_iterator xlow= cosThetaKBins.begin(); xlow!=cosThetaKBins.end(); xlow++ ) cout << (*xlow) << " " << endl;
-
-  RooBinning cosThetaKBinning(cosThetaKBins.size()-1,cosThetaKBins_);
-  cout << "RooBinning L: " << cosThetaKBinning.lowBound() << " H: " << cosThetaKBinning.highBound() << " N " << cosThetaKBinning.numBoundaries() << endl;
-
-  TH2* hh21 = (TH2*)pippo.createHistogram("hh21",ctK,Binning(cosThetaKBinning),YVar(ctL,Binning(cosThetaLBinning)));
-
-  // P l o t   a n d   f i t   a   R o o D a t a H i s t
+  // P l o t   a n d   f i t   a   R o o D a t a P d f
   // ---------------------------------------------------
-  RooPlot* framex = ctK.frame(Title("Imported TH1 with Poisson error bars")) ;
-  pippo.plotOn(framex) ; 
-  RooPlot* framey = ctL.frame(Title("Imported TH1 with Poisson error bars")) ;
-  pippo.plotOn(framey) ; 
+  xyEff.plotOn(framex) ; 
+  xyEff.plotOn(framey) ; 
 
-  TCanvas* c = new TCanvas("rf102_dataimport","rf102_dataimport",800,800) ;
-  c->Divide(2,2);
+  TCanvas* c = new TCanvas(caName,caTitle,800,800) ;
+  c->Divide(3,2);
   c->cd(1);
   gPad->SetLeftMargin(0.15) ;
   framex->GetYaxis()->SetTitleOffset(1.4) ;
@@ -109,15 +137,229 @@ void loadHisto() {
   framey->GetYaxis()->SetTitleOffset(1.4) ;
   framey->Draw() ;
   c->cd(3);
-  H2Deff_0_q2Bin_Org_2->Draw("lego2 fp");
+  h2Orig->Draw("lego2 fp");
+  c->cd(4);
+  hh21Histo->Draw("lego2 fp");
+  c->cd(5);
+  hh21Pdf->Draw("lego2 fp");
+
+  c->Print(caName+Form("_q2bin_%d.pdf",q2bin));
+}
+
+void createHistPdf(unsigned int q2bin, bool doPlot=false) {
+  cout << "createHistPdf " << q2bin << endl;
+  // Define variables
+
+  // ctK.setVal(0.1);
+  // ctL.setVal(0.1);
+  // phi.setVal(0.1);
+
+
+  RooArgList ctKctL(ctK,ctL);
+  RooDataHist hist_ctKctL("hist_ctKctL","hist_ctKctL", ctKctL, Import(*H2Deff_ctK_ctL,kTRUE));
+  RooHistPdf pdf_ctKctL("pdf_ctKctL","pdf_ctKctL", ctKctL, hist_ctKctL, 0);
+  //cout << "pdf_ctKctL( " << ctK.getVal() << "," << ctL.getVal() << " ) = " << pdf_ctKctL.getVal() << endl;
+  if (doPlot) plotHisto(q2bin, pdf_ctKctL,hist_ctKctL);
+  pdf2DOutputFile->cd();
+  pdf_ctKctL.Write(Form("pdf_ctKctL_q2bin%d",q2bin));
+
+  RooArgList ctKphi(ctK,phi);
+  RooDataHist hist_ctKphi("hist_ctKphi","hist_ctKphi", ctKphi, Import(*H2Deff_ctK_phi,kTRUE));
+  RooHistPdf pdf_ctKphi("pdf_ctKphi","pdf_ctKphi", ctKphi, hist_ctKphi, 0);
+  //cout << "pdf_ctKphi( " << ctK.getVal() << "," << phi.getVal() << " ) = " << pdf_ctKphi.getVal() << endl;
+  if (doPlot) plotHisto(q2bin, pdf_ctKphi,hist_ctKphi);
+  pdf2DOutputFile->cd();
+  pdf_ctKphi.Write(Form("pdf_ctKphi_q2bin%d",q2bin));
+
+  RooArgList ctLphi(ctL,phi);
+  RooDataHist hist_ctLphi("hist_ctLphi","hist_ctLphi", ctLphi, Import(*H2Deff_ctL_phi,kTRUE));
+  RooHistPdf pdf_ctLphi("pdf_ctLphi","pdf_ctLphi", ctLphi, hist_ctLphi, 0);
+  //cout << "pdf_ctLphi( " << ctL.getVal() << "," << phi.getVal() << " ) = " << pdf_ctLphi.getVal() << endl;
+  if (doPlot) plotHisto(q2bin, pdf_ctLphi,hist_ctLphi);
+  pdf2DOutputFile->cd();
+  pdf_ctLphi.Write(Form("pdf_ctLphi_q2bin%d",q2bin));
+
+}
+
+void createHistPdfCtKCtL() {
+
+  //cout << "creating RooDataHist " << H2Deff_ctK_ctL << endl ;
+  // Create a RooDataHist from a TH2D: how?
+  RooDataHist pippo("pippo","pippo", ctKctL, Import(*H2Deff_ctK_ctL,kTRUE));
+  //cout << " done " << endl;
+
+  // Represent data in dh as pdf in x
+  RooHistPdf histpdf1("histpdf1","histpdf1", ctKctL, pippo, 0);
+  histpdf1.setUnitNorm(kFALSE);
+
+  TH2* hh21 = (TH2*)pippo.createHistogram("hh21",ctK,Binning(cosThetaKBinning),YVar(ctL,Binning(cosThetaLBinning)));
+
+  TH2* hh21Pdf = (TH2*)histpdf1.createHistogram("hh21Pdf",ctK,Binning(cosThetaKBinning),YVar(ctL,Binning(cosThetaLBinning)));
+
+  // P l o t   a n d   f i t   a   R o o D a t a H i s t
+  // ---------------------------------------------------
+  RooPlot* framex = ctK.frame(Title("Imported TH1 with Poisson error bars")) ;
+  pippo.plotOn(framex) ; 
+  RooPlot* framey = ctL.frame(Title("Imported TH1 with Poisson error bars")) ;
+  pippo.plotOn(framey) ; 
+  // RooPlot* framePdf = new RooPlot(ctK,ctL) ;
+  // histpdf1.plotOn(framePdf) ;
+
+  TCanvas* c = new TCanvas("CosThetaKVsCosThetaL","CosThetaKVsCosThetaL",800,800) ;
+  c->Divide(3,2);
+  c->cd(1);
+  gPad->SetLeftMargin(0.15) ;
+  framex->GetYaxis()->SetTitleOffset(1.4) ;
+  framex->Draw() ;
+  c->cd(2);
+  gPad->SetLeftMargin(0.15) ;
+  framey->GetYaxis()->SetTitleOffset(1.4) ;
+  framey->Draw() ;
+  c->cd(3);
+  H2Deff_ctK_ctL->Draw("lego2 fp");
   c->cd(4);
   hh21->Draw("lego2 fp");
-  //effHistoFile->Close();
+  c->cd(5);
+  hh21Pdf->Draw("lego2 fp");
+
+  // TFile* pdf2DOutputFile = new TFile("pdf2DOutputFile.root", "RECREATE");
+  // histpdf1.Write();
+  // pdf2DOutputFile->Close();
+  // //effHistoFile->Close();
+  // TFile* pdf2D_in = new TFile("pdf2DOutputFile.root", "READ");
+  // RooHistPdf* histpdf2 = (RooHistPdf*) pdf2D_in->Get("histpdf1");
+  // TH2* hh22Pdf = (TH2*)histpdf2->createHistogram("hh22Pdf",ctK,Binning(cosThetaKBinning),YVar(ctL,Binning(cosThetaLBinning)));
+  // c->cd(6);
+  // hh22Pdf->Draw("lego2 fp");
+  
+}
+
+void createHistPdfCtKPhi() {
+
+  //cout << "creating RooDataHist " << H2Deff_ctK_ctL << endl ;
+  // Create a RooDataHist from a TH2D: how?
+  RooDataHist pippo("pippo","pippo", ctKphi, Import(*H2Deff_ctK_phi,kTRUE));
+  //cout << " done " << endl;
+
+  // Represent data in dh as pdf in x
+  RooHistPdf histpdf1("histpdf1","histpdf1", ctKphi, pippo, 0);
+  histpdf1.setUnitNorm(kFALSE);
+
+  TH2* hh21 = (TH2*)pippo.createHistogram("hh21",ctK,Binning(cosThetaKBinning),YVar(phi,Binning(phiBinning)));
+
+  TH2* hh21Pdf = (TH2*)histpdf1.createHistogram("hh21Pdf",ctK,Binning(cosThetaKBinning),YVar(phi,Binning(phiBinning)));
+
+  // P l o t   a n d   f i t   a   R o o D a t a H i s t
+  // ---------------------------------------------------
+  RooPlot* framex = ctK.frame(Title("Imported TH1 with Poisson error bars")) ;
+  pippo.plotOn(framex) ; 
+  RooPlot* framey = phi.frame(Title("Imported TH1 with Poisson error bars")) ;
+  pippo.plotOn(framey) ; 
+  // RooPlot* framePdf = new RooPlot(ctK,ctL) ;
+  // histpdf1.plotOn(framePdf) ;
+
+  TCanvas* c = new TCanvas("CosThetaKVsPhi","CosThetaKVsPhi",800,800) ;
+  c->Divide(3,2);
+  c->cd(1);
+  gPad->SetLeftMargin(0.15) ;
+  framex->GetYaxis()->SetTitleOffset(1.4) ;
+  framex->Draw() ;
+  c->cd(2);
+  gPad->SetLeftMargin(0.15) ;
+  framey->GetYaxis()->SetTitleOffset(1.4) ;
+  framey->Draw() ;
+  c->cd(3);
+  H2Deff_ctK_phi->Draw("lego2 fp");
+  c->cd(4);
+  hh21->Draw("lego2 fp");
+  c->cd(5);
+  hh21Pdf->Draw("lego2 fp");
+
+  // TFile* pdf2DOutputFile = new TFile("pdf2DOutputFile.root", "RECREATE");
+  // histpdf1.Write();
+  // pdf2DOutputFile->Close();
+  // //effHistoFile->Close();
+  // TFile* pdf2D_in = new TFile("pdf2DOutputFile.root", "READ");
+  // RooHistPdf* histpdf2 = (RooHistPdf*) pdf2D_in->Get("histpdf1");
+  // TH2* hh22Pdf = (TH2*)histpdf2->createHistogram("hh22Pdf",ctK,Binning(cosThetaKBinning),YVar(phi,Binning(phiBinning)));
+  // c->cd(6);
+  // hh22Pdf->Draw("lego2 fp");
+  
+}
+
+void createHistPdfCtLPhi() {
+  //cout << "creating RooDataHist " << H2Deff_ctK_ctL << endl ;
+  // Create a RooDataHist from a TH2D: how?
+  RooDataHist pippo("pippo","pippo", ctLphi, Import(*H2Deff_ctL_phi,kTRUE));
+  //cout << " done " << endl;
+
+  // Represent data in dh as pdf in x
+  RooHistPdf histpdf1("histpdf1","histpdf1", ctLphi, pippo, 0);
+  histpdf1.setUnitNorm(kFALSE);
+
+  TH2* hh21 = (TH2*)pippo.createHistogram("hh21",ctL,Binning(cosThetaLBinning),YVar(phi,Binning(phiBinning)));
+
+  TH2* hh21Pdf = (TH2*)histpdf1.createHistogram("hh21Pdf",ctL,Binning(cosThetaLBinning),YVar(phi,Binning(phiBinning)));
+
+  // P l o t   a n d   f i t   a   R o o D a t a H i s t
+  // ---------------------------------------------------
+  RooPlot* framex = ctL.frame(Title("Imported TH1 with Poisson error bars")) ;
+  pippo.plotOn(framex) ; 
+  RooPlot* framey = phi.frame(Title("Imported TH1 with Poisson error bars")) ;
+  pippo.plotOn(framey) ; 
+  // RooPlot* framePdf = new RooPlot(ctK,ctL) ;
+  // histpdf1.plotOn(framePdf) ;
+
+  TCanvas* c = new TCanvas("CosThetaLVsPhi","CosThetaLVsPhi",800,800) ;
+  c->Divide(3,2);
+  c->cd(1);
+  gPad->SetLeftMargin(0.15) ;
+  framex->GetYaxis()->SetTitleOffset(1.4) ;
+  framex->Draw() ;
+  c->cd(2);
+  gPad->SetLeftMargin(0.15) ;
+  framey->GetYaxis()->SetTitleOffset(1.4) ;
+  framey->Draw() ;
+  c->cd(3);
+  H2Deff_ctL_phi->Draw("lego2 fp");
+  c->cd(4);
+  hh21->Draw("lego2 fp");
+  c->cd(5);
+  hh21Pdf->Draw("lego2 fp");
+
+  // TFile* pdf2DOutputFile = new TFile("pdf2DOutputFile.root", "RECREATE");
+  // histpdf1.Write();
+  // pdf2DOutputFile->Close();
+  // //effHistoFile->Close();
+  // TFile* pdf2D_in = new TFile("pdf2DOutputFile.root", "READ");
+  // RooHistPdf* histpdf2 = (RooHistPdf*) pdf2D_in->Get("histpdf1");
+  // TH2* hh22Pdf = (TH2*)histpdf2->createHistogram("hh22Pdf",ctK,Binning(cosThetaKBinning),YVar(phi,Binning(phiBinning)));
+  // c->cd(6);
+  // hh22Pdf->Draw("lego2 fp");
+  
+}
+
+void loadBinning() {
+  vector<double> cosThetaKBins;
+  vector<double> cosThetaLBins;
+  vector<double> phiBins;
+  Utility->ReadAllBins(Utility->MakeAnalysisPATH(PARAMETERFILEIN).c_str(),&q2Bins,&cosThetaKBins,&cosThetaLBins,&phiBins,"goodTag");
+  q2Bins_        = Utility->MakeBinning(&q2Bins);
+  cosThetaKBins_ = Utility->MakeBinning(&cosThetaKBins);
+  cosThetaLBins_ = Utility->MakeBinning(&cosThetaLBins);
+  phiBins_       = Utility->MakeBinning(&phiBins);
+  cosThetaLBinning=RooBinning (cosThetaLBins.size()-1,cosThetaLBins_);
+  cosThetaLBinning.Print();
+  cosThetaKBinning=RooBinning (cosThetaKBins.size()-1,cosThetaKBins_);
+  cosThetaKBinning.Print();
+  phiBinning=RooBinning(phiBins.size()-1,phiBins_);
+  phiBinning.Print();
+
 }
 
 int main(int argc, char** argv)
 {
-  if (argc >= 0)
+  if (argc > 0)
     {
       // ##################
       // # Main variables #
@@ -125,14 +367,48 @@ int main(int argc, char** argv)
 
 	TApplication theApp ("Applications", &argc, argv);
 	Utility = new Utils(false);
-	Utility->ReadAllBins(Utility->MakeAnalysisPATH(PARAMETERFILEIN).c_str(),&q2Bins,&cosThetaKBins,&cosThetaLBins,&phiBins,"goodTag");
-      loadHisto();
-      theApp.Run ();
+      loadBinning();
+
+      unsigned int q2BinIndx=2;
+      bool doPlot=false;
+      bool doBatch=false;
+      if ( argc > 1 ) q2BinIndx = atoi(argv[1]);
+      if ( argc > 2 ) doPlot = atoi(argv[2]);
+      if ( argc > 3 ) doBatch = atoi(argv[3]);
+
+
+      // createHistPdfCtKCtL();
+      // createHistPdfCtKPhi();
+      // createHistPdfCtLPhi();
+
+      if (doBatch == true) {
+        cout << "\n[" << argv[0] << "::main]\t@@@ Setting batch mode @@@" << endl;
+        gROOT->SetBatch(true);
+      }
+
+      // Open output file
+      pdf2DOutputFile = new TFile("pdf2DOutputFile.root", "RECREATE");
+
+      // do all q2Bins at once
+      if (q2BinIndx==0) {
+        for (q2BinIndx=1; q2BinIndx<9; ++q2BinIndx) {
+          // Open input file
+          loadEffHisto(q2BinIndx);
+          createHistPdf(q2BinIndx,doPlot);
+        }
+      }
+      else {
+        // do only one q2 bin
+        loadEffHisto(q2BinIndx);
+      }
+
+      pdf2DOutputFile->Close();
+      theApp.Run();
     }
   else
     {
       cout << "Parameter missing: " << endl;
-      cout << "./" << argv[0] << endl;
+      cout << "./" << argv[0] << " <q2Bin (0 for all)> " << " <plot histo (decfault=0/no)> " << " <batch running (default=0/no) " << endl;
 
       
       return EXIT_FAILURE;
