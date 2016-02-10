00dd3251c6ee075953a545a36c858434fb525328
Stefano Lacaprara <stefano.lacaprara@pd.infn.it>
2016-02-10 11:20:56 +0100

    plot also sig and bkg separately

--- B0KstMuMu/plugins/readToyDataset.C
+++ B0KstMuMu/plugins/readToyDataset.C
@@ -10,13 +10,17 @@ void readToyDataset() {
   RooRealVar* ctK = w->var("ctK") ;
   RooRealVar* ctL = w->var("ctL") ;
   RooRealVar* phi = w->var("phi") ;
+  B0mass->setBins(20);
+  ctK->setBins(20);
+  ctL->setBins(20);
+  phi->setBins(20);
 
   for (int q2Bin=1; q2Bin<=9; ++q2Bin) {
     if (q2Bin==5 || q2Bin==7) continue;
     RooDataSet* sig1 = (RooDataSet*)w->data(Form("toy0_sig%d",q2Bin)) ;
     RooDataSet* bkg1 = (RooDataSet*)w->data(Form("toy0_bkg%d",q2Bin)) ;
 
-    RooDataSet* all1 = sig1;
+    RooDataSet* all1 = sig1->Clone();
     all1->append(*bkg1);
 
     RooPlot* B0massFrame = B0mass->frame() ;
@@ -25,10 +29,16 @@ void readToyDataset() {
     RooPlot* phiFrame = phi->frame() ;
 
     // // Overlay the background component of model with a dashed line
-    // sig1->plotOn(B0massFrame,MarkerStyle(20),MarkerColor(kRed)) ;
+    sig1->plotOn(B0massFrame,MarkerStyle(22),MarkerColor(kRed)) ;
+    sig1->plotOn(ctLFrame,MarkerStyle(22),MarkerColor(kRed)) ;
+    sig1->plotOn(ctKFrame,MarkerStyle(22),MarkerColor(kRed)) ;
+    sig1->plotOn(phiFrame,MarkerStyle(22),MarkerColor(kRed)) ;
 
     // // Overlay the background+sig2 components of model with a dotted line
-    // bkg1->plotOn(B0massFrame,MarkerStyle(24),MarkerColor(kBlue)) ;
+    bkg1->plotOn(B0massFrame,MarkerStyle(23),MarkerColor(kBlue)) ;
+    bkg1->plotOn(ctLFrame,MarkerStyle(23),MarkerColor(kBlue)) ;
+    bkg1->plotOn(ctKFrame,MarkerStyle(23),MarkerColor(kBlue)) ;
+    bkg1->plotOn(phiFrame,MarkerStyle(23),MarkerColor(kBlue)) ;
 
     // Overlay the background+sig2 components of model with a dotted line
     all1->plotOn(B0massFrame,MarkerStyle(20),MarkerColor(kBlack)) ;
